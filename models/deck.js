5338dd0f2a56000c75a5ac9dc763e92d93a6c130
ninaweddige <s0578004@htw-berlin.de>
2024-05-08 17:12:24 +0200

    Notification info on deck details page, small style changes

--- models/deck.js
+++ models/deck.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema;
 const deckSchema = new Schema({
     name: {type: String, minLength: 1},
     description: String,
-    cards: { type: [Schema.Types.ObjectId], ref: "Card", default: []},
+    cards: [],
     times_studied: {type: Number, min: 0, default: 0},
     date_created: {type: Date, default: Date.now},
     last_studied: {type:Date, default: undefined},
     notification: {
         enabled: {type: Boolean, default: false},
         start_date: {type: Date, default: Date.now},
-        days_between: {type: Number, required: true, min: 1, default: 1}
+        days_between: {type: Number, min: 1, default: 1}
     }
 });
 
