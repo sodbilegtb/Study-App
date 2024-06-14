222de7896602bf69e05a66f6b14886b954550f35
ninaweddige <s0578004@htw-berlin.de>
2024-06-14 10:47:18 +0200

    Delete route for cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cardController = require("../controllers/cardController");
+const methodOverride = require("method-override");
 const router = express.Router();
 
+router.use(methodOverride("_method", {
+    methods: ["POST", "GET"]
+}));
+
 router.get("/", cardController.listCards);
+router.get("/create", cardController.showCardCreateForm);
+router.post("/create", cardController.createCard);
 router.get("/:id", cardController.showCardDetails);
 router.get("/:id/edit", cardController.getCardEditForm);
 router.post("/:id/edit", cardController.postCardEditForm);
-router.post("/:id/delete", cardController.deleteCard);
-router.get("/create", cardController.showCardCreateForm);
-router.post("/create", cardController.createCard);
+router.delete("/:id/delete", cardController.deleteCard, cardController.redirectView);
+
 
 module.exports = router;
